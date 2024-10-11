ab9ae4fbbd9b14aa7b35b6770c78b833d18dbbf0
Antonio <90718059+tonymochila@users.noreply.github.com>
2024-10-11 11:37:18 -0300

    Update forno.ino

--- forno.ino
+++ forno.ino
@@ -9,24 +9,30 @@ CS      |	GPIO 23
 SO      |	GPIO 19
 */
 #include <Servo.h>
-//////////////////////////////////////////////////////////
+////////////////////termopar//////////////////////////////////////
 #include "max6675.h"
 int thermoDO = 19;
 int thermoCS = 23;
 int thermoCLK = 5;
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
-//////////////////////////////////////////////////////////
+///////////////////pinos///////////////////////////////////////
 #define botton 12
+#define led1 4
+#define led2 5
+#define led2 reler
 bool estado=0,botaoEstadoAnterior=1;
 unsigned long ultimoTempo;
 int debounce=1000,leituraBotao;
-
+////////////////////////servo motor/////////////////////////////////////////////
 Servo meuServo; // Cria um objeto Servo para controlar o servo motor
 int pos; // Declara uma variável para controlar a posição do servo motor 
 
 void setup()
 {
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(reler,OUTPUT);
   meuServo.attach(6); // Associa o servo motor ao pino digital 6 do Arduino
   meuServo.write(0);
   
@@ -55,11 +61,14 @@ void loop()
   }
 }
 void start(){
+ digitalWrite(led1,HIGH);
+ digitalWrite(led2,LOW); 
     //Serial.println("Ligado");
 }
 void desligar(){
- 
   //Serial.println("Desligado");
-
-  meuServo.write(0);
+ digitalWrite(reler,HIGH);
+ digitalWrite(led2,HIGH);
+ digitalWrite(led1,LOW);  
+ meuServo.write(0);
 }
